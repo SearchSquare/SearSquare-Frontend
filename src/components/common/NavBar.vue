cbd2caf4dac543973a7f3300fa9b6be35a1404a0
yjs9714 <yjs9714@gmail.com>
2024-05-15 23:32:19 +0900

    메인페이지를 추가한다

---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="navbar navbar-expand-sm bg-body-tertiary fixed-top">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <a class="navbar-brand" href="#"
+            ><img
+              style="border-radius: 20px"
+              src="/src/assets/Logo.png"
+              alt="Map"
+              class="img-fluid"
+            />서치스퀘어</a
+          >
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">API문서</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                style="border-radius: 20px"
+                src="/src/assets/Logo.png"
+                alt="Map"
+                class="img-fluid"
+              />
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><a class="dropdown-item" href="#">마이페이지</a></li>
+              <li><a class="dropdown-item" href="#">로그아웃</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script></script>
+
+<style scoped>
+/* Add any scoped styles here if needed */
+.navbar-nav > li {
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 10px;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,42 @@
+<script setup>
+import NavBar from '@/components/common/NavBar.vue';
+import SelectBox from '@/components/mainpage/SelectBox.vue';
+import Map from '@/components/common/Map.vue';
+</script>
+
+<template>
+  <div>
+    <NavBar />
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 left-column">
+        <h2>아파트를 검색해 보세요!</h2>
+        <p>1248 results</p>
+        <SelectBox />
+        <img src="/src/assets/main_picture.gif" class="responsive-image" />
+      </div>
+      <div class="col-md-6 right-column">
+        <Map />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 120px;
+}
+
+.left-column {
+  text-align: left; /* 왼쪽 정렬 */
+}
+
+.right-column {
+  text-align: right; /* 오른쪽 정렬 */
+}
+.responsive-image {
+  max-width: 80%;
+  height: auto;
+}
+</style>
