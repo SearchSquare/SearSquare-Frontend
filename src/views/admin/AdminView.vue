cec22e0b4b20a646d2c61cafc4fccdddfefbf64a
SEOP-YOON <75006671+SEOP-YOON@users.noreply.github.com>
2024-05-21 23:19:33 +0900

    feat: 관리자페이지 views 생성 (#28)

---
+++ src/views/admin/AdminView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import SideBar from '@/components/admin/SideBar.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <div class="d-flex">
+    <div class="sidebar">
+      <SideBar />
+    </div>
+    <div class="content flex-grow-1 p-3">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.d-flex {
+  display: flex;
+  height: 100vh;
+}
+
+.sidebar {
+  width: 250px;
+  background-color: #f8f9fa;
+  height: calc(100vh - 66px); /* Adjust if necessary */
+  position: fixed; /* Make sidebar fixed */
+  top: 66px;
+}
+
+.content {
+  margin-top: 66px; /* Adjust if necessary */
+  margin-left: 250px; /* Adjust width based on sidebar width */
+  width: calc(100% - 250px); /* Adjust width based on sidebar width */
+}
+</style>
