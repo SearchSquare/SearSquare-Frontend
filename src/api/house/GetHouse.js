743b55ae428adac6f09e719623273ae6eb553578
yjs <yjs9714@gmail.com>
2024-05-22 17:55:09 +0900

    feat: 로그인 블락 추가

--- src/api/house/GetHouse.js
+++ src/api/house/GetHouse.js
@@ -2,18 +2,28 @@ import { localAxios } from '@/util/http-common.js';
 
 const local = localAxios();
 
-function getFirstHouseDeal(houseId) {
-  const response = local.get(`/house/deal/${houseId}`, {
-    params: { size: 5 },
-  });
-  return response;
+async function getFirstHouseDeal(houseId) {
+  try {
+    local.defaults.headers['authorization'] = sessionStorage.getItem('accessToken');
+    const response = await local.get(`/house/deal/${houseId}`, {
+      params: { size: 5 },
+    });
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function getSecondHouseDeal(houseId, lastdealId) {
-  const response = local.get(`/house/deal/${houseId}`, {
-    params: { size: 5, 'last-deal-id': lastdealId },
-  });
-  return response;
+async function getSecondHouseDeal(houseId, lastDealId) {
+  try {
+    local.defaults.headers['authorization'] = sessionStorage.getItem('accessToken');
+    const response = await local.get(`/house/deal/${houseId}`, {
+      params: { size: 5, 'last-deal-id': lastDealId },
+    });
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export { getFirstHouseDeal, getSecondHouseDeal };
